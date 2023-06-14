b77cd651a53ca1564f0dfe326b42b8db696003dd
Weffersson Lima <121705618+weffersson@users.noreply.github.com>
2023-06-13 21:32:26 -0300

    Update index.js

--- seach/src/scripts/index.js
+++ seach/src/scripts/index.js
@@ -1,58 +1,31 @@
 import { products } from './database.js';
 
-function createProductList(products) {
+function createProductList(products)
+{
   const mainElement = document.createElement('div');
   mainElement.classList.add('product-list');
-
-  // Define a largura das colunas e a altura das linhas
   mainElement.style.display = 'grid';
   mainElement.style.gridTemplateColumns = 'repeat(3, 1fr)';
   mainElement.style.gridGap = '20px';
-  
-  products.forEach(product => {
+
+  products.forEach(product =>
+  {
     const productElement = document.createElement('div');
     productElement.classList.add('product');
 
-    const imageElement = document.createElement('div');
-    imageElement.classList.add('product__image');
-
-    const discountElement = document.createElement('div');
-    discountElement.classList.add('product__discount');
-    const discountText = document.createTextNode('30% OFF');
-    discountElement.appendChild(discountText);
-    imageElement.appendChild(discountElement);
-
-    const imgElement = document.createElement('img');
-    imgElement.src = product.imageUrl;
-    imgElement.alt = '';
-    imageElement.appendChild(imgElement);
-
-    productElement.appendChild(imageElement);
-
-    const sectionElement = document.createElement('div');
-    sectionElement.classList.add('product__section');
-
-    const nameElement = document.createElement('div');
-    nameElement.classList.add('product__name');
-    const nameText = document.createTextNode(product.name);
-    nameElement.appendChild(nameText);
-    sectionElement.appendChild(nameElement);
-
-    const priceElement = document.createElement('div');
-    priceElement.classList.add('product__price');
-    const originalPriceElement = document.createElement('del');
-    originalPriceElement.classList.add('product__price--original');
-    const originalPriceText = document.createTextNode(`$${product.originalPrice}`);
-    originalPriceElement.appendChild(originalPriceText);
-    priceElement.appendChild(originalPriceElement);
-    const discountedPriceElement = document.createElement('div');
-    discountedPriceElement.classList.add('product__price--discounted');
-    const discountedPriceText = document.createTextNode(`$${product.discountedPrice}`);
-    discountedPriceElement.appendChild(discountedPriceText);
-    priceElement.appendChild(discountedPriceElement);
-    sectionElement.appendChild(priceElement);
-
-    productElement.appendChild(sectionElement);
+    productElement.innerHTML = `
+      <div class="product__image">
+        <div class="product__discount">30% OFF</div>
+        <img src="${product.imageUrl}" alt="">
+      </div>
+      <div class="product__section">
+        <div>${product.name}</div>
+        <div class="product__price">
+          <del>$${product.originalPrice}</del>
+          <div class="product__price--discounted">$${product.discountedPrice}</div>
+        </div>
+      </div>
+    `;
 
     mainElement.appendChild(productElement);
   });
@@ -60,8 +33,6 @@ function createProductList(products) {
   return mainElement;
 }
 
-
 const productListElement = createProductList(products);
-
 document.body.appendChild(productListElement);
 
