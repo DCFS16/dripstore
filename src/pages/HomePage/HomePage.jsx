66f0cb8b137121bb6d48b6a31bb49272a5ba7a3a
Prof. Carlos <carlosdigital16@gmail.com>
2023-07-15 11:22:21 -0300

    Estruturando páginas e componentes

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import MainHeader from '../../components/MainHeader/MainHeader'
+import './HomePage.css'
+
+const HomePage = () => (
+    <>
+        <MainHeader />
+        <h1 className="title">Home Page</h1>
+    </>
+)
+
+
+export default HomePage
