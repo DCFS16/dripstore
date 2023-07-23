76b8a3ce66971aba1eb6510307df1f79348df60d
Samantha <samthvet@hotmail.com>
2023-07-23 10:32:51 -0300

    organizando oferta especial

---
+++ src/components/SpecialOffer/SpecialOffer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import ShoesImage from "../../assets/shoes.png";
+import "./SpecialOffer.css";
+
+const SpecialOffer = () => (
+  <section className="special-offer">
+    <div className="shoes-image-wrapper">
+      <div className="shoes">
+        <img className="shoes-image" src={ShoesImage} />
+      </div>
+    </div>
+
+    <div className="special-offer-text">
+      <h4 className="title">Oferta especial</h4>
+      <h3 className="title">Air Jordan edição de colecionador</h3>
+      <p className="text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip
+      </p>
+      <button>Ver Oferta</button>
+    </div>
+  </section>
+);
+
+export default SpecialOffer;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import MainHeader from '../../components/MainHeader/MainHeader'
+import SpecialOffer from '../../components/SpecialOffer/SpecialOffer'
 import './HomePage.css'
 
 const HomePage = () => (
     <>
         <MainHeader />
         <h1 className="title">Home Page</h1>
+        <SpecialOffer />
     </>
 )
 
