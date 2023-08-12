ce1591886f6362bf4a420c2a74cc08e70f452631
Prof. Carlos <carlosdigital16@gmail.com>
2023-08-12 15:06:29 -0300

    Adicionando lista de produtos com api mock

---
+++ src/hooks/useProducts.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import { API_URL } from '../constants'
+
+export const useProducts = (limit) => {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(`${API_URL}/products`)
+            .then(response => response.json())
+            .then(data => {
+                setLoading(false)
+                setProducts(data.slice(0, limit))
+            })
+    }, [])
+
+
+    return {
+        products,
+        loading
+    }
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import MainHeader from '../../components/MainHeader/MainHeader'
 import Destaque from '../../components/HomeHighlight/Highlight'
+import { HomeProducts } from '../../components/HomeProducts/HomeProducts'
 import './HomePage.css'
 
 const HomePage = () => (
@@ -7,6 +8,7 @@ const HomePage = () => (
         <MainHeader />
         <h1 className="title">Home Page</h1>
         <Destaque />
+        <HomeProducts />
     </>
 )
 
