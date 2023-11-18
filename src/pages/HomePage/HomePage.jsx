875b4644d75c4359eaeabf1e26b70d164307f253
jpedro002 <joaopsilvavolei@gmail.com>
2023-11-18 11:30:52 -0300

    fix: deleting empty space

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import Destaque from '../../components/HomeHighlight/Highlight'
 import SpecialOffer from '../../components/SpecialOffer/SpecialOffer'
-
 import './HomePage.css'
 import { Carrosel } from '../../components/Carrosel/Carrosel'
 
