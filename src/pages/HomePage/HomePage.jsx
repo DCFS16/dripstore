b43bc0b0c1d19a44e7aa61da49214e9db4a9acfc
jpedro002 <joaopsilvavolei@gmail.com>
2023-12-02 10:14:39 -0300

    fix: import product list

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import Destaque from '../../components/HomeHighlight/Highlight'
 import SpecialOffer from '../../components/SpecialOffer/SpecialOffer'
 import './HomePage.css'
 import { Carrosel } from '../../components/Carrosel/Carrosel'
+import ProductList from '../../components/ProductList/ProductList'
 
 const HomePage = () => (
     <>
