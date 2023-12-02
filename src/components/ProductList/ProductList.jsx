317e142c87442aa60f409ff8638c49b2da9abf3f
João Pedro <122836400+jpedro002@users.noreply.github.com>
2023-12-02 10:43:52 -0300

    Product list (#21)
    * Criando componente lista de produtos da main page, falta somente ajustes de layout.
    
    * refactoring: card product
    
    * fix: name of file
    
    * fix: import product list
    
    * fix: rename file
    
    ---------
    
    Co-authored-by: Rodrigo Galeno <rodrigogaleno@hotmail.com>

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./ProductList.css";
+import { CardProduct } from "./ProductCard";
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+const ProductList = () => (
+  <>
+    <section className="product-list">
+      <div className="products-page">
+        <h1 className="section-name">Produtos em alta</h1>
+        <a className="products-list-all" href="">
+          Ver Todos
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="19"
+            fill="none"
+          >
+            <path
+              stroke="#C92071"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.8"
+              d="M15.5 7.5 20 12m0 0-4.5 4.5M20 12H4"
+            />
+          </svg>
+        </a>
+      </div>
+      <div className="image-wrapper">
+        {myArray.map((item) => (
+          <CardProduct />
+        ))}
+      </div>
+    </section>
+  </>
+);
+
+export default ProductList;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,11 +2,13 @@ import Destaque from '../../components/HomeHighlight/Highlight'
 import SpecialOffer from '../../components/SpecialOffer/SpecialOffer'
 import './HomePage.css'
 import { Carrosel } from '../../components/Carrosel/Carrosel'
+import ProductList from '../../components/ProductList/ProductList'
 
 const HomePage = () => (
     <>
         <Carrosel/>
         <Destaque />
+        <ProductList />
         <SpecialOffer />
     </>
 
